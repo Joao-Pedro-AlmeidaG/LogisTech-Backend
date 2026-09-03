211a1d9a9da2f920ab6b5134256ed67ef12f5b6b
Carlos Magalhães <carloseduardobr19@gmail.com>
2026-09-02 21:52:36 -0300

    Removendo pasta duplicada LogisTech-Backend

--- LogisTech-Backend/src/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const env = require('../config/env');
-
-const generateToken = (payload) => {
-  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: '1d' });
-};
-
-module.exports = generateToken;
--- LogisTech-Backend/src/utils/generateTrackingCode.js
+++
@@ -1,6 +0,0 @@
-const generateTrackingCode = () => {
-  const randomDigits = Math.floor(100000 + Math.random() * 900000);
-  return `LOG-${randomDigits}`;
-};
-
-module.exports = generateTrackingCode;
